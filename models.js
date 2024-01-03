c59c8e2523f1fbec9ed47b5cb8b0c73bc669460d
bersat <doom_fox754@mail.ru>
2024-01-03 10:15:50 +0300

    hbhbl

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 const sequilize = require('./dtb')
-const {} = require
-const User = sequelize.define('user', {
+const {DataTypes} = require('sequelize')
+const User = sequilize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     chatId: {type: DataTypes.STRING, unique: true},
     right: {type: DataTypes.INTEGER, defaultValue: 0},
